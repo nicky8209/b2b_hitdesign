1ddf802879b12035293fa10c04b05c2271dbc9c9
nicky8209 <nicky8209@naver.com>
2023-08-09 10:30:32 +0900

    Original Source

---
+++ b2b_hitdesign/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace b2b_hitdesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "▶배송비 선불로 변경 부탁드립니다!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "▶배송비세금계산서요청입니다!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 157);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "▶취소건예치금환불입니다!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 232);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(344, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "▶금일 발주건 취소로 삭제 부탁드립니다!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 307);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(326, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "▶금일 발주건 메모 추가 부탁드립니다!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(598, 46);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 102);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(598, 46);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(9, 177);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(598, 46);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(9, 252);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(598, 46);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(9, 327);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(598, 46);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(9, 376);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(509, 91);
+            this.textBox6.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 376);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 90);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "모두\r\n지우기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 476);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "히트가구b2b";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ b2b_hitdesign/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace b2b_hitdesign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = textBox1.Text;
+                string str1 = str.Substring(str.IndexOf("주문번호 : ") + 7, 16);
+                string str2 = str.Substring(str.IndexOf("수령자명\t") + 5, str.IndexOf(" 수령자정보수정") - (str.IndexOf("수령자명\t") + 5));
+                string str3 = str.Substring(str.IndexOf("배송지 주소\t") + 15, str.IndexOf("배송메시지\t") - (str.IndexOf("배송지 주소\t") + 15));
+                string[] str4 = str3.Split(' ');
+                string str5 = null;
+                string str6 = str.Substring(str.IndexOf("주문/CS\r\n") + 7, str.IndexOf("결제 정보\r\n") - (str.IndexOf("주문/CS\r\n") + 7));
+                string[] str9 = str6.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                for (int i = 0; i < str9.Length; i++)
+                {
+                    if (str9[i].Length > 6)
+                    {
+                        if (str9[i].Substring(1, 6) == "공급사상품명")
+                        {
+                            str5 += "/" + str9[i - 2] + " " + str9[i + 2];
+
+                        }
+
+                    }
+
+                }
+
+                textBox6.Text = "▶배송비 선불로 변경 부탁드립니다!" + "\r\n" + str1 + "\r\n[" + str2 + "/" + str4[0] + " " + str4[1] + str5 + "]\r\n > ";
+
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+            {
+                System.Console.WriteLine($"Error: {argumentOutOfRangeException.Message}");
+
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = textBox2.Text;
+                string str1 = str.Substring(str.IndexOf("주문번호 : ") + 7, 16);
+                string str2 = str.Substring(str.IndexOf("주문자명(ID)\t") + 9);
+                string[] str3 = str2.Split(' ');
+                string str4 = str.Substring(str.IndexOf("수령자명\t") + 5, 3);
+
+                textBox6.Text = "▶배송비세금계산서요청입니다!" + "\r\n" + str1 + "\r\n" + str3[0] + "" + str3[1] + "/" + "빈칸" + "/" + str4;
+
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+            {
+                System.Console.WriteLine($"Error: {argumentOutOfRangeException.Message}");
+
+            }
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = textBox3.Text;
+                string str1 = str.Substring(str.IndexOf("주문번호 : ") + 7, 16);
+                string month = str1.Substring(4, 2);
+                string day = str1.Substring(6, 2);
+                string str2 = str.Substring(str.IndexOf("주문자명(ID)\t") + 9);
+                string[] str3 = str2.Split(' ');
+                string str4 = str.Substring(str.IndexOf("수령자명\t") + 5, 3);
+                string str5 = str.Substring(str.IndexOf("환불금액\t") + 5, str.IndexOf("환불 금액 상세\t") - (str.IndexOf("환불금액\t") + 5) - 1);
+
+                textBox6.Text = "▶취소건예치금환불입니다!" + "\r\n" + month + "/" + day + "일결제/" + str3[0] + " " + str3[1] + "/" + str5 + "원/" + str4 + " 고객 취소건예치금환불입니다!";
+
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+            {
+                System.Console.WriteLine($"Error: {argumentOutOfRangeException.Message}");
+
+            }
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = textBox4.Text;
+                string str1 = str.Substring(str.IndexOf("주문번호 : ") + 7, 16);
+                string str2 = str.Substring(str.IndexOf("수령자명\t") + 5, str.IndexOf(" 수령자정보수정") - (str.IndexOf("수령자명\t") + 5));
+                string str3 = str.Substring(str.IndexOf("배송지 주소\t") + 15, str.IndexOf("배송메시지\t") - (str.IndexOf("배송지 주소\t") + 15));
+                string[] str4 = str3.Split(' ');
+                string str5 = null;
+                string str6 = str.Substring(str.IndexOf("주문/CS\r\n") + 7, str.IndexOf("결제 정보\r\n") - (str.IndexOf("주문/CS\r\n") + 7));
+                string[] str9 = str6.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                for (int i = 0; i < str9.Length; i++)
+                {
+                    if (str9[i].Length > 6)
+                    {
+                        if (str9[i].Substring(1, 6) == "공급사상품명")
+                        {
+                            str5 += "/" + str9[i - 2] + " " + str9[i + 2];
+
+                        }
+
+                    }
+
+                }
+
+                textBox6.Text = "▶금일 발주건 취소로 삭제 부탁드립니다!" + "\r\n" + str1 + "\r\n[" + str2 + "/" + str4[0] + " " + str4[1] + str5 + "]\r\n > ";
+
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+            {
+                System.Console.WriteLine($"Error: {argumentOutOfRangeException.Message}");
+
+            }
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = textBox5.Text;
+                string str1 = str.Substring(str.IndexOf("주문번호 : ") + 7, 16);
+                string str2 = str.Substring(str.IndexOf("수령자명\t") + 5, str.IndexOf(" 수령자정보수정") - (str.IndexOf("수령자명\t") + 5));
+                string str3 = str.Substring(str.IndexOf("배송지 주소\t") + 15, str.IndexOf("배송메시지\t") - (str.IndexOf("배송지 주소\t") + 15));
+                string[] str4 = str3.Split(' ');
+                string str5 = null;
+                string str6 = str.Substring(str.IndexOf("주문/CS\r\n") + 7, str.IndexOf("결제 정보\r\n") - (str.IndexOf("주문/CS\r\n") + 7));
+                string[] str9 = str6.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                for (int i = 0; i < str9.Length; i++)
+                {
+                    if (str9[i].Length > 6)
+                    {
+                        if (str9[i].Substring(1, 6) == "공급사상품명")
+                        {
+                            str5 += "/" + str9[i - 2] + " " + str9[i + 2];
+
+                        }
+
+                    }
+
+                }
+
+                textBox6.Text = "▶금일 발주건 메모 추가 부탁드립니다!" + "\r\n" + str1 + "\r\n[" + str2 + "/" + str4[0] + " " + str4[1] + str5 + "]\r\n > ";
+
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+            {
+                System.Console.WriteLine($"Error: {argumentOutOfRangeException.Message}");
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+    }
+}
